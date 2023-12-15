04400e13d4a1e4b6bed4b815ad5637b8b19a9950
chen-huaneng <huanengchen@foxmail.com>
2023-12-15 11:50:22 +0800

    Modify some method and add coin brick, mushroom brick.

--- src/engine/core/LevelGenerator.java
+++ src/engine/core/LevelGenerator.java
@@ -1,5 +1,7 @@
 package engine.core;
 
+import engine.sprites.Mario;
+
 import java.util.Random;
 
 public class LevelGenerator {
@@ -166,19 +168,24 @@ public class LevelGenerator {
         return length;
     }
 
+    /**
+     * @param model 地图
+     * @param x0 起始位置
+     * @param x1 终止位置
+     * @param y 生成高度*/
     private void addEnemyLine(MarioLevelModel model, int x0, int x1, int y) {
+        // 定义敌人的种类
         char[] enemies = new char[]{MarioLevelModel.GOOMBA,
                 MarioLevelModel.GREEN_KOOPA,
                 MarioLevelModel.RED_KOOPA,
                 MarioLevelModel.SPIKY};
-        for (int x = x0; x < x1; x++) {
+        // 在指定范围内生成敌人
+        for (int x = x0; x < x1; ++x) {
+            // 判断是否生成敌人
             if (random.nextInt(35) < difficulty + 1) {
-                int type = random.nextInt(4);
-                if (difficulty < 1) {
-                    type = 0;
-                } else if (difficulty < 3) {
-                    type = 1 + random.nextInt(3);
-                }
+                // 选择敌人的类型,根据难度设置不同的敌人
+                int type = difficulty < 1 ? 0 : random.nextInt(3) + 1;
+                // 在地图上设置敌人,根据难度判断是否给敌人加上翅膀
                 model.setBlock(x, y, MarioLevelModel.getWingedEnemyVersion(enemies[type], random.nextInt(35) < difficulty));
             }
         }
@@ -219,83 +226,96 @@ public class LevelGenerator {
         return length;
     }
 
+    /** 生成直线地形
+     * @param model 地图
+     * @param xo 直线地面的起始坐标
+     * @param maxLength 直线地面最大长度
+     * @param safe 是否生成安全地面，如果是，则直线地面长度会更长
+     * @return 返回直线地面的长度*/
     private int buildStraight(MarioLevelModel model, int xo, int maxLength, boolean safe) {
-        int length = random.nextInt(10) + 2;
-        if (safe) {
-            length = 10 + random.nextInt(5);
-        }
+        // 随机生成直线的长度
+        // 如果是安全地面则长度更长
+        int length = safe ? 10 + random.nextInt(5) : random.nextInt(10) + 2;
 
+        // 如果直线的长度超过最大长度则设为最大长度
         if (length > maxLength) {
             length = maxLength;
-
         }
 
+        // 设置地面的高度
         int floor = model.getHeight() - 1 - random.nextInt(4);
-        for (int x = xo; x < xo + length; x++) {
-            for (int y = 0; y < model.getHeight(); y++) {
-                if (y >= floor) {
-                    model.setBlock(x, y, MarioLevelModel.GROUND);
-                }
+        // 从起始位置开始设置地面
+        for (int x = xo; x < xo + length; ++x) {
+            // 保证地面在一定的高度
+            for (int y = floor; y < model.getHeight(); ++y) {
+                model.setBlock(x, y, MarioLevelModel.GROUND);
             }
         }
 
-        if (!safe) {
-            if (length > 5) {
-                decorate(model, xo, xo + length, floor);
-            }
+        if (!safe && length > 5) {
+            decorate(model, xo, xo + length, floor);
         }
 
         return length;
     }
 
+    /** 添加一些装饰，包括敌人、金币和砖块
+     * @param model 地图
+     * @param x0 起始位置
+     * @param x1 终止位置
+     * @param floor 地板的高度*/
     private void decorate(MarioLevelModel model, int x0, int x1, int floor) {
-        if (floor < 1)
+        // 如果地板高度不足则返回
+        if (floor < 1) {
             return;
+        }
 
-        boolean rocks = true;
+        // 添加敌人
         addEnemyLine(model, x0 + 1, x1 - 1, floor - 1);
 
-        int s = random.nextInt(4);
-        int e = random.nextInt(4);
+        // 金币生成的范围
+        int begin = x0 + 2 + random.nextInt(4);
+        int end = x1 - 2 - random.nextInt(4);
 
-        if (floor - 2 > 0) {
-            if ((x1 - 1 - e) - (x0 + 1 + s) > 1) {
-                for (int x = x0 + 1 + s; x < x1 - 1 - e; x++) {
-                    model.setBlock(x, floor - 2, MarioLevelModel.COIN);
-                }
+        // 在当前地板的上方添加金币,并且地板的长度要在一定的范围内
+        if (floor > 3 && end - begin > 1) {
+            for (int x = begin; x < end; ++x) {
+                model.setBlock(x, floor - 2, MarioLevelModel.COIN);
             }
         }
 
-        s = random.nextInt(4);
-        e = random.nextInt(4);
+        // 更高位置的砖块和金币生成的范围
+        int newBegin = x0 + 1 + random.nextInt(4);
+        int newEnd = x1 - 1 - random.nextInt(4);
 
-        if (floor - 4 > 0) {
-            if ((x1 - 1 - e) - (x0 + 1 + s) > 2) {
-                for (int x = x0 + 1 + s; x < x1 - 1 - e; x++) {
-                    if (rocks) {
-                        if (x != x0 + 1 && x != x1 - 2 && random.nextInt(3) == 0) {
-                            if (random.nextInt(4) == 0) {
-                                model.setBlock(x, floor - 4, MarioLevelModel.NORMAL_BRICK);
-                            } else {
-                                model.setBlock(x, floor - 4, MarioLevelModel.NORMAL_BRICK);
-                            }
-                        } else if (random.nextInt(4) == 0) {
-                            if (random.nextInt(4) == 0) {
-                                model.setBlock(x, floor - 4, MarioLevelModel.COIN);
-                            } else {
-                                model.setBlock(x, floor - 4, MarioLevelModel.COIN);
-                            }
-                        } else {
-                            model.setBlock(x, floor - 4, MarioLevelModel.COIN);
-                        }
-                    }
+        // 如果地板的高度更高，在当前地板的更上方生成砖块或者金币,并且地板的长度要在一定的范围
+        if (floor > 4 && newEnd - newBegin > 2) {
+            for (int x = newBegin; x < newEnd; ++x) {
+                // 存储类型，默认为金币
+                char type = MarioLevelModel.COIN;
+
+                // 生成砖块或者金币的概率
+                int rate = random.nextInt(10);
+
+                // 如果不在左边界和右边界上，则生成砖块，并且有5/10的概率
+                if (x != x0 + 1 && x != x1 - 2 && rate < 5) {
+                    type = MarioLevelModel.NORMAL_BRICK;
+                } else if (rate < 7) { // 有2/10的概率生成含有金币的方块
+                    type = MarioLevelModel.COIN_BRICK;
+                } else if (rate == 7) { // 有1/10的概率生成含有蘑菇的方块
+                    type = MarioLevelModel.MUSHROOM_BRICK;
                 }
+                // 剩下2/10的概率生成金币
+                model.setBlock(x, floor - 4, type);
             }
         }
     }
 
+    /**
+     * @param model 控制地图的高度和长度
+     * @return 返回地图*/
     public String getGeneratedLevel(MarioLevelModel model) {
-        // 清空地图
+        // 清空地图（初始化地图）
         model.clearMap();
 
         // 初始化地图静态物体的数据
@@ -314,11 +334,13 @@ public class LevelGenerator {
         }
 
         for (int i = 0; i < odds.length; i++) {
-            // 防止概率小于0
+            // 概率修正，防止概率小于0
             if (odds[i] < 0) {
                 odds[i] = 0;
             }
+            // 计算总概率
             totalOdds += odds[i];
+            // 计算累积概率，也即odd[i]前的概率之和，使得概率较高的值有较大被选中的概率
             odds[i] = totalOdds - odds[i];
         }
 
--- src/engine/core/MarioLevelModel.java
+++ src/engine/core/MarioLevelModel.java
@@ -2,66 +2,68 @@ package engine.core;
 
 public class MarioLevelModel {
     //start and end of the level
+    // 空白
     public static final char EMPTY = '-';
 
     //game tiles symbols
+    // 地面方块
     public static final char GROUND = 'X';
+    // 普通砖块
     public static final char NORMAL_BRICK = 'S';
+    // 含有金币的方块
+    public static final char COIN_BRICK = 'C';
+    // 含有蘑菇的方块
+    public static final char MUSHROOM_BRICK = 'U';
+    // 金币
     public static final char COIN = 'o';
+    // 空管道
     public static final char PIPE = 't';
+    // 带有食人花的管道
     public static final char PIPE_FLOWER = 'T';
+    // 子弹，其中顶部的'*'将会是子弹头
     public static final char BULLET_BILL = '*';
+    // 跳过平台的背景
     public static final char PLATFORM_BACKGROUND = '|';
+    // 跳过平台
     public static final char PLATFORM = '%';
 
     //enemies that can be in the level
+    // 蘑菇怪
     public static final char GOOMBA = 'g';
+    // 有翅膀的蘑菇怪
     public static final char GOOMBA_WINGED = 'G';
+    // 红色库巴
     public static final char RED_KOOPA = 'r';
+    // 带有翅膀的红色库巴
     public static final char RED_KOOPA_WINGED = 'R';
+    // 绿色库巴
     public static final char GREEN_KOOPA = 'k';
+    // 带有翅膀的绿色库巴
     public static final char GREEN_KOOPA_WINGED = 'K';
+    // 尖刺
     public static final char SPIKY = 'y';
+    // 带有翅膀的尖刺
     public static final char SPIKY_WINGED = 'Y';
 
     /**
-     * Get the correct version of the enemy char
+     * 获取正确代表敌人的相应字符
      *
-     * @param enemy  the enemy character
-     * @param winged boolean to indicate if its a winged enemy
-     * @return correct character based on winged
+     * @param enemy  代表敌人的字符
+     * @param winged 判断是否有翅膀
+     * @return 返回正确类型的敌人
      */
     public static char getWingedEnemyVersion(char enemy, boolean winged) {
-        if (!winged) {
-            if (enemy == GOOMBA_WINGED) {
-                return GOOMBA;
-            }
-            if (enemy == GREEN_KOOPA_WINGED) {
-                return GREEN_KOOPA;
-            }
-            if (enemy == RED_KOOPA_WINGED) {
-                return RED_KOOPA;
-            }
-            if (enemy == SPIKY_WINGED) {
-                return SPIKY;
-            }
-            return enemy;
-        }
-        if (enemy == GOOMBA) {
-            return GOOMBA_WINGED;
-        }
-        if (enemy == GREEN_KOOPA) {
-            return GREEN_KOOPA_WINGED;
-        }
-        if (enemy == RED_KOOPA) {
-            return RED_KOOPA_WINGED;
-        }
-        if (enemy == SPIKY) {
-            return SPIKY_WINGED;
-        }
-        return enemy;
+        // 判断是否带翅膀
+        return switch (enemy) {
+            case GOOMBA_WINGED -> winged ? GOOMBA : enemy;
+            case GREEN_KOOPA_WINGED -> winged ? GREEN_KOOPA : enemy;
+            case RED_KOOPA_WINGED -> winged ? RED_KOOPA : enemy;
+            case SPIKY_WINGED -> winged ? SPIKY : enemy;
+            default -> enemy;
+        };
     }
 
+    // 存储地图的长度和宽度，第一维是地图的高，第二维是地图的长
     private final char[][] map;
 
     /**
@@ -104,7 +106,7 @@ public class MarioLevelModel {
      * @return map height
      */
     public int getHeight() {
-        // 获取地图的长度
+        // 获取地图的高度
         return this.map[0].length;
     }
 
@@ -134,10 +136,9 @@ public class MarioLevelModel {
      */
     public void setBlock(int x, int y, char value) {
         // 判断是否越界
-        if (x < 0 || y < 0 || x > this.map.length - 1 || y > this.map[0].length - 1) {
-            return;
+        if ((x >= 0 && x < getWidth()) && (y >= 0 && y < getHeight())) {
+            this.map[x][y] = value;
         }
-        this.map[x][y] = value;
     }
 
     /**
